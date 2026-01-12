0c18bc1eaa3b4710a33417578c9d779eca19b7f4
Agus Cahyono <cahyo.mamen@gmail.com>
2026-01-12 10:31:32 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,100 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "HyperioniOS",
-    platforms: [
-        .iOS(.v9)
-    ],
-    products: [
-        // Core framework
-        .library(
-            name: "HyperionCore",
-            targets: ["HyperionCore"]),
-        
-        // Plugin - Attributes Inspector
-        .library(
-            name: "AttributesInspector",
-            targets: ["AttributesInspector"]),
-        
-        // Plugin - Measurements
-        .library(
-            name: "Measurements",
-            targets: ["Measurements"]),
-        
-        // Plugin - Slow Animations
-        .library(
-            name: "SlowAnimations",
-            targets: ["SlowAnimations"]),
-        
-        // Convenience product that includes all plugins
-        .library(
-            name: "HyperioniOS",
-            targets: ["HyperionCore", "AttributesInspector", "Measurements", "SlowAnimations"]),
-    ],
-    dependencies: [],
-    targets: [
-        // Core Target
-        .target(
-            name: "HyperionCore",
-            dependencies: [],
-            path: "Core",
-            exclude: ["Info.plist"],
-            resources: [
-                .process("Resources")
-            ],
-            publicHeadersPath: "Public",
-            cSettings: [
-                .headerSearchPath("Public"),
-                .headerSearchPath("Private"),
-            ]
-        ),
-        
-        // Attributes Inspector Plugin
-        .target(
-            name: "AttributesInspector",
-            dependencies: ["HyperionCore"],
-            path: "AttributesInspector",
-            exclude: ["Info.plist"],
-            resources: [
-                .process("Resources")
-            ],
-            publicHeadersPath: "Public",
-            cSettings: [
-                .headerSearchPath("Public"),
-                .headerSearchPath("Private"),
-            ]
-        ),
-        
-        // Measurements Plugin
-        .target(
-            name: "Measurements",
-            dependencies: ["HyperionCore"],
-            path: "Measurements",
-            exclude: ["Info.plist"],
-            resources: [
-                .process("Resources")
-            ],
-            publicHeadersPath: "Public",
-            cSettings: [
-                .headerSearchPath("Public"),
-                .headerSearchPath("Private"),
-            ]
-        ),
-        
-        // Slow Animations Plugin
-        .target(
-            name: "SlowAnimations",
-            dependencies: ["HyperionCore"],
-            path: "SlowAnimations",
-            exclude: ["Info.plist"],
-            resources: [
-                .process("Resources")
-            ],
-            publicHeadersPath: "Public",
-            cSettings: [
-                .headerSearchPath("Public"),
-                .headerSearchPath("Private"),
-            ]
-        ),
-    ]
+  name: "Hyperion-iOS",
+  platforms: [
+     .iOS(.v9)
+  ],
+  products: [
+    .library(
+      name: "Hyperion-iOS",
+      targets: ["HyperionCore", "SlowAnimations", "Measurements", "AttributesInspector"]
+    )
+  ],
+  targets: [
+    .target(name: "HyperionCore"),
+    .target(name: "SlowAnimations"),
+    .target(name: "Measurements"),
+    .target(name: "AttributesInspector")
+  ]
 )
